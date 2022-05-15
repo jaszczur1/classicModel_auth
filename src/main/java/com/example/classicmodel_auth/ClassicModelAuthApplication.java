04df57106cb4775c833f081212ef006df6407d28
Michał Jadczak <m1jadczak.gmail.com>
2022-05-16 00:50:35 +0200

    .

---
+++ src/main/java/com/example/classicmodel_auth/ClassicModelAuthApplication.java
@@ -0,0 +1,13 @@
+package com.example.classicmodel_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassicModelAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassicModelAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/classicmodel_auth/ClassicModelAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.classicmodel_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassicModelAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
